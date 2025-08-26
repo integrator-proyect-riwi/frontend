ee15b62139e3b702c61c846b003da07f17795cbf
abrahan villa <ajvilla1999@gmail.com>
2025-08-26 14:05:11 -0500

    add style box info

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -18,9 +18,64 @@ export async function dashboardPage() {
           </a>
         </div>
       </div>
+
+      <div id="" class="grid grid-cols-4 gap-5 px-5">
+        <div class="w-full border border-gray-300 p-4 rounded-xl">
+          <div class="flex justify-between">
+            <h3 class="font-medium text-sm">Solicitudes Totales</h3>
+            <i data-lucide="file-text" class="w-4 h-4 text-blue-500"></i>
+          </div>
+          <div class="flex flex-col mt-3">
+            <span class="font-extrabold text-lg">147</span>
+            <p class="text-sm">
+              <span class="text-green-600">+12%</span>
+               desde el mes pasado
+            </p>
+          </div>
+        </div>  
+        <div class="w-full border border-gray-300 p-4 rounded-xl">
+          <div class="flex justify-between">
+            <h3 class="font-medium text-sm">Solicitudes Totales</h3>
+            <i data-lucide="clock" class="w-4 h-4 text-red-500"></i>
+          </div>
+          <div class="flex flex-col mt-3">
+            <span class="font-extrabold text-lg">23</span>
+            <p class="text-sm">
+              <span class="text-green-600">+12%</span>
+               desde el mes pasado
+            </p>
+          </div>
+        </div>  
+        <div class="w-full border border-gray-300 p-4 rounded-xl">
+          <div class="flex justify-between">
+            <h3 class="font-medium text-sm">Solicitudes Totales</h3>
+            <i data-lucide="circle-check-big" class="w-4 h-4 text-green-500"></i>
+          </div>
+          <div class="flex flex-col mt-3">
+            <span class="font-extrabold text-lg">89</span>
+            <p class="text-sm">
+              <span class="text-green-600">+12%</span>
+               desde el mes pasado
+            </p>
+          </div>
+        </div>  
+        <div class="w-full border border-gray-300 p-4 rounded-xl">
+          <div class="flex justify-between">
+            <h3 class="font-medium text-sm">Solicitudes Totales</h3>
+            <i data-lucide="circle-x" class="w-4 h-4 text-red-500"></i>
+          </div>
+          <div class="flex flex-col mt-3">
+            <span class="font-extrabold text-lg">12</span>
+            <p class="text-sm">
+              <span class="text-green-600">+12%</span>
+               desde el mes pasado
+            </p>
+          </div>
+        </div>  
+      </div>
     </div>
   `;
 
-    console.log("Dashboard cargado correctamente");
+    console.info("Dashboard cargado correctamente");
     createIcons({ icons });
 }
